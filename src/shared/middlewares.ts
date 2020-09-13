8ccc6839f68b998d6881aa85df513fe08ddd3bff
ansskf <anss.khaled00@gmail.com>
2020-09-13 10:59:02 +0200

    Add joi validation middleware

---
+++ src/shared/middlewares.ts
@@ -0,0 +1,17 @@
+import { RequestHandler, Request, Response, NextFunction } from 'express';
+import { BAD_REQUEST } from 'http-status-codes';
+import { Schema, ValidationErrorItem } from 'joi';
+
+export const JoiValidatorMiddleware = (joiSchema: Schema): RequestHandler => {
+    return async (req: Request, res: Response, next: NextFunction) => {
+        const body = req.body
+        try{
+            const validated_data = await joiSchema.validateAsync(body)
+            req.body = validated_data
+            next()
+        }catch(e){
+            const errors = e.details.map((err: ValidationErrorItem) => ({message: err.message}))
+            return res.status(BAD_REQUEST).json({'status': 'error', 'data': errors});
+        }
+    }
+}
